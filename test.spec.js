2339862efad0d938502f30d949f1191ba0bd301a
jaehyunkim0210 <5668andy@gmail.com>
2022-04-21 20:44:24 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,10 @@
+describe('default test set',()=>{
+    it('test1 should be passed',()=>{
+        console.log('test1 passed')
+    });
+
+    it('test2 should be passed',()=>{
+        console.log('test1 passed')
+    });
+});
+
